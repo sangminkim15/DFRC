b5f7a6121d6002540fa0da1056c3da45bb52d838
Sangmin Kim <smgeem@korea.ac.kr>
2022-02-18 20:23:13 +0900

    220218 Codes Added
    1. Bisection Search Algorithm added
    2. Radar Beampattern Plotting Code added

--- [LZhou18] Optimal Waveform Design for Dual-functional MIMO Radar-Communication Systems/BisectionSearchConvergence.m
+++ [LZhou18] Optimal Waveform Design for Dual-functional MIMO Radar-Communication Systems/BisectionSearchConvergence.m
@@ -1,4 +1,4 @@
-function [X] = BisectionSearch (Q, G, lambda_low, lambda_high, p)
+function [X] = BisectionSearchConvergence (Q, G, lambda_low, lambda_high, p)
 
 Norm = p.L * p.Pt;
 lambda = (lambda_low + lambda_high) / 2;
@@ -6,7 +6,12 @@ lambda = (lambda_low + lambda_high) / 2;
 X = (Q + lambda * eye(p.N, p.N))^(-1) * G;
 NormTemp = norm(X * X', 'fro')^2;
 
+idx = 0;
+idxarray = 0;
+Normarray = NormTemp;
+
 while abs(NormTemp - Norm) >= 1e-6
+    idx = idx + 1;
     if NormTemp < Norm
         lambda_high = lambda;
         lambda = (lambda_low + lambda_high) / 2;
@@ -23,6 +28,14 @@ while abs(NormTemp - Norm) >= 1e-6
         
     end
     
+    idxarray = horzcat(idxarray, idx);
+    Normarray = horzcat(Normarray, NormTemp);
+    
 end
+
+plot(idxarray, Normarray, 'LineWidth', 1.5);
+title('');
+xlabel('# of iterations');
+ylabel('|X(\lambda)|^2');
 
 end
--- [LZhou18] Optimal Waveform Design for Dual-functional MIMO Radar-Communication Systems/main.m
+++ [LZhou18] Optimal Waveform Design for Dual-functional MIMO Radar-Communication Systems/main.m
@@ -1,20 +1,93 @@
 function [] = main ()
 
 % Parameter Settings
-p.K = 30;       % # of Users
-p.N = 64;       % # of Antennas per Each Users (ULA)
-p.T = 70;       % # of Communication Frame
+p.K = 30;                           % # of Users
+p.N = 64;                           % # of Antennas per Each Users (ULA)
+p.L = 70;                           % # of Communication Frame
+p.Pt = 1;                           % Total Power Constraint
+p.N0dB = 5 : -1 : -5;               % Noise Settings
+p.N0 = 10.^(p.N0dB ./ 10);  
+p.SNR = p.Pt ./ p.N0;
+p.SNRdB = 10 * log(p.SNR ./ 10);    % SNR Settings
 
-p.Pt = 1;                   % Total Power Constraint
-p.N0dB = -5 : 1 : 5;        % Noise Settings
-p.N0 = 10.^(p.N0dB / 10);   % SNR Settings
+p.theta = -pi/4 : pi/180 : pi/4;    % Radar ULA Angle Settings
 
+p.rho = 0.1;                        % Weighting Factor
 
-% Channel Realization
-H = (1/sqrt(2)) * (randn([p.K, p.N]) + 1i * randn([p.K, p.N]));
+% Simulation Settings
+p.iterations = 1000;
 
-% Omni-Directional Beampattern Design
-Rd = () * eye(p.N);
+OmniStrictCapacityArray = zeros(p.iterations, length(p.SNRdB));
+OmniTradeoffCapacityArray = zeros(p.iterations, length(p.SNRdB));
+OmniStrictBPArray = zeros(p.iterations, length(p.theta));
+OmniTradeoffBPArray = zeros(p.iterations, length(p.theta));
 
+for idx = 1 : p.iterations
+    % Channel Realization
+    H = (1/sqrt(2)) * (randn([p.K, p.N]) + 1i * randn([p.K, p.N]));
+
+    % Desired Signal Matrix - 4QAM Modulation
+    S = (1/sqrt(2)) * ((2 * randi([0 1], p.K, p.L) - ones(p.K, p.L)) + 1i * ((2 * randi([0 1], p.K, p.L) - ones(p.K, p.L))));
+
+    % Omni-Directional Beampattern Design
+    OmniRd = (p.Pt / p.N) * eye(p.N, p.N);
+
+    % Optimal Waveform Design
+    F = chol(OmniRd);                   % Cholesky Factorization
+    [U, ~, V] = svd(F * H' * S);        % SVD (singular value decomposition)
+    
+    OmniXStrict = sqrt(p.L) * F' * U * eye(p.N, p.L) * V';
+
+    % Trade-off Between Radar and Communication Performances
+    Q = p.rho * (H' * H) + (1 - p.rho) * eye(p.N, p.N);
+    G = p.rho * H' * S + (1 - p.rho) * OmniXStrict;
+
+    [P, LAMBDA] = eig(Q);               % Eigenvalue, Eigenvector of Matrix Q
+
+    lambda_low = - min(diag(LAMBDA));
+    lambda_high = - min(diag(LAMBDA)) + sqrt(p.N / p.Pt) * max(max(abs(P' * G)));
+
+    OmniXTradeoff = BisectionSearch(Q, G, lambda_low, lambda_high, p);
+    
+    
+    % Communication Capacity
+    
+    
+    
+    % Radar Beampattern
+    for jdx = 1 : length(p.theta)
+        a = zeros(p.N, 1);
+        
+        for kdx = 1 : p.N
+            a(kdx, 1) = exp(1i * pi * (kdx - 1) * sin(p.theta(jdx)));
+        end
+        
+        OmniStrictBPArray(idx, jdx) = a' * (1/p.L) * (OmniXStrict * OmniXStrict') * a;
+        OmniTradeoffBPArray(idx, jdx) = a' * (1/p.L) * (OmniXTradeoff * OmniXTradeoff') * a;
+        
+    end
+    
+end
+
+% Communication Plot
+AWGNCapacity = log(1 + p.SNRdB);
+OmniStrictCapacity = mean(OmniStrictCapacityArray);
+OmniTradeoffCapacity = mean(OmniTradeoffCapacityArray);
+
+% figure
+% plot(p.SNRdB, AWGNCapacity, p.SNRdB, OmniStrictCapacity, p.SNRdB, OmniTradeoffCapacity, 'LineWidth', 1.5);
+% xlabel('Transmit SNR (dB)');
+% ylabel('Average Achievable Rate (bps/Hz/user)');
+% legend('AWGN Capacity', 'Omni-Strict', 'Omni-Tradeoff (\rho = 0.1)');
+
+% Radar Plot
+OmniStrictBP = mean(abs(OmniStrictBPArray));
+OmniTradeoffBP = mean(abs(OmniTradeoffBPArray));
+
+figure
+plot(p.theta, OmniStrictBP, p.theta, OmniTradeoffBP, 'LineWidth', 1.5);
+xlabel('Angle (rad)');
+ylabel('Beampattern');
+legend('Omni-Strict', 'Omni-Tradeoff (\rho = 0.1)');
 
 end
